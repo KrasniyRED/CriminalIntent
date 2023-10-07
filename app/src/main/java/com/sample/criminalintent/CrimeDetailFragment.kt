ec01e71771a79baf5ac0bfdbeb6a306d36f70694
Ivan Bedov <freginder@mail.ru>
2023-10-07 16:29:51 +0700

    override func onCreateView and onViewCreated create fragment container in activity_main.xml

--- app/src/main/java/com/sample/criminalintent/CrimeDetailFragment.kt
+++ app/src/main/java/com/sample/criminalintent/CrimeDetailFragment.kt
@@ -1,12 +1,18 @@
 package com.sample.criminalintent
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import com.sample.criminalintent.databinding.FragmentCrimeDetailBinding
 import java.util.Date
 import java.util.UUID
 
 class CrimeDetailFragment: Fragment() {
     private lateinit var crime: Crime
+    private lateinit var binding: FragmentCrimeDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         crime = Crime(
@@ -16,4 +22,34 @@ class CrimeDetailFragment: Fragment() {
             isSolved = false
         )
     }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding =
+            FragmentCrimeDetailBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            crimeTitle.doOnTextChanged { text, _, _, _ ->
+                crime = crime.copy(title = text.toString())
+            }
+
+            crimeDate.apply {
+                text = crime.date.toString()
+                isEnabled = false
+            }
+
+            crimeSolved.setOnCheckedChangeListener { _, isChecked ->
+                crime = crime.copy(isSolved = isChecked)
+            }
+
+        }
+    }
+
+
 }
