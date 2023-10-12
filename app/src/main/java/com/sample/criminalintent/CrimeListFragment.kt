749161c05293796a41fa0a848c5a0f796a66265c
Ivan Bedov <freginder@mail.ru>
2023-10-12 12:36:37 +0700

    Calling coroutines from CrimeListFragment

--- app/src/main/java/com/sample/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/sample/criminalintent/CrimeListFragment.kt
@@ -7,8 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.sample.criminalintent.databinding.FragmentCrimeListBinding
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 private const val TAG = "CrimeListFragment"
 class CrimeListFragment : Fragment() {
@@ -21,6 +24,9 @@ class CrimeListFragment : Fragment() {
         }
 
     private val crimeListViewModel: CrimeListViewModel by viewModels()
+
+    private var job: Job? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
@@ -40,6 +46,19 @@ class CrimeListFragment : Fragment() {
         binding.crimeRecyclerView.adapter = adapter
 
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        job = viewLifecycleOwner.lifecycleScope.launch {
+            val crimes = crimeListViewModel.loadCrimes()
+            binding.crimeRecyclerView.adapter = CrimeListAdapter(crimes)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        job?.cancel()
     }
 
     override fun onDestroyView() {
